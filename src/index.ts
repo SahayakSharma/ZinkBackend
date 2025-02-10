4c31734c9b91e029fd511a9cea387c9bae4fea12
SahayakSharma <sahayaksharma6@gmail.com>
2025-02-10 21:19:06 +0530

    refix

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ app.use(cors())
 app.get("/", (req, res) => {
     res.send("Hello")
 })
-server.listen(3000, "0.0.0.0", () => {
+server.listen(3000, () => {
     console.log("Server is running")
 })
 
