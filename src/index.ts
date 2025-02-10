00b3c000855a809a0b981b2e21d9fbd45eef4ab7
SahayakSharma <sahayaksharma6@gmail.com>
2025-02-10 19:21:33 +0530

    refix

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ const app = express();
 app.use(cors({ 
     origin: "*", 
     methods: ["GET", "POST"], 
-    allowedHeaders: ["Content-Type"] 
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "*",  // Change this to your frontend URL if needed
         methods: ["GET", "POST"],
-        allowedHeaders: ["Content-Type"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
     }
 });
